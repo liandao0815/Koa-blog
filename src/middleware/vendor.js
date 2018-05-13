ebffa11c19867fd4e936f8297e25eb96d5814d7b
liandao0815 <13126053290@163.com>
2018-05-14 00:17:53 +0800

    modify public-path

--- src/middleware/vendor.js
+++ src/middleware/vendor.js
@@ -5,8 +5,8 @@ import favicon from 'koa-favicon'
 import compress from 'koa-compress'
 
 export default app => {
-  app.use(serve(path.resolve(__dirname, '../public/')))
-  app.use(favicon(path.resolve(__dirname, '../public/favicon.ico')))
+  app.use(serve(path.resolve(__dirname, '../../public/')))
+  app.use(favicon(path.resolve(__dirname, '../../public/favicon.ico')))
   app.use(body({ multipart: true }))
   app.use(compress())
 }
